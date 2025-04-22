f431c0f4c0beea035758f7ccd39f49f667f539e7
Mustafa Olmezses <mustafaolmezses@gmail.com>
2025-04-22 09:33:46 +0300

    Initial Commit

---
+++ GoalsAndHabitTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoalsAndHabitTracker
+//
+//  Created by Mustafa Ölmezses on 22.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoalsAndHabitTracker/GoalsAndHabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalsAndHabitTrackerApp.swift
+//  GoalsAndHabitTracker
+//
+//  Created by Mustafa Ölmezses on 22.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct GoalsAndHabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
